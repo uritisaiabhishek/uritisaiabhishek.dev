1347d1d21440703d833a7138d6cb7807e3272348
uritisaiabhishek <uritisaiabhishek@gmail.com>
2024-09-15 00:38:57 +0530

    sitemap generation

--- sitemap-generator.js
+++ sitemap-generator.js
@@ -1,8 +1,26 @@
-const Sitemap = require('react-router-sitemap').default;
-const router = require('./src/App').default; // Import your React Router
+import { Sitemap } from 'react-router-sitemap';
+import router from './src/App'; // Import your React Router
+
+const pathsConfig = {
+  '/portfolio/:projectName': [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+  ],
+  '/blog/:blogTitle': [
+    { id: 1 },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+  ],
+};
 
 function generateSitemap() {
   return new Sitemap(router)
+    .applyParams(pathsConfig)
     .build('https://uritisaiabhishek.dev') // Replace with your website's URL
     .save('./public/sitemap.xml'); // The path where sitemap.xml will be saved
 }
