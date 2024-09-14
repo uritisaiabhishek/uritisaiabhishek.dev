3e69bfb1d7a5c1aa0c89eb7f9d75fa8eeae362be
uritisaiabhishek <uritisaiabhishek@gmail.com>
2024-09-15 01:14:02 +0530

    removed urls for social media in contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -28,13 +28,13 @@ function Contact() {
                             
                             
                             <ul className="list-unstyled social-icon text-light mb-0 mt-4 d-flex gap-2 flex-wrap">
-                                <li className="list-inline-item mb-0"><a href="https://1.envato.market/landrick" target="_blank" className="rounded"><i className="uil uil-shopping-cart align-middle" title="Buy Now"></i></a></li>
-                                <li className="list-inline-item mb-0"><a href="https://dribbble.com/shreethemes" target="_blank" className="rounded"><i className="uil uil-dribbble align-middle" title="dribbble"></i></a></li>
-                                <li className="list-inline-item mb-0"><a href="https://www.behance.net/shreethemes" target="_blank" className="rounded"><i className="uil uil-behance align-middle" title="behance"></i></a></li>
-                                <li className="list-inline-item mb-0"><a href="https://www.facebook.com/shreethemes" target="_blank" className="rounded"><i className="uil uil-facebook-f align-middle" title="facebook"></i></a></li>
-                                <li className="list-inline-item mb-0"><a href="https://www.instagram.com/shreethemes/" target="_blank" className="rounded"><i className="uil uil-instagram align-middle" title="instagram"></i></a></li>
-                                <li className="list-inline-item mb-0"><a href="https://twitter.com/shreethemes" target="_blank" className="rounded"><i className="uil uil-twitter align-middle" title="twitter"></i></a></li>
-                                <li className="list-inline-item mb-0"><a href="mailto:support@shreethemes.in" className="rounded"><i className="uil uil-envelope align-middle" title="email"></i></a></li>
+                                <li className="list-inline-item mb-0"><a href="#" target="_blank" className="rounded"><i className="uil uil-shopping-cart align-middle" title="Buy Now"></i></a></li>
+                                <li className="list-inline-item mb-0"><a href="#" target="_blank" className="rounded"><i className="uil uil-dribbble align-middle" title="dribbble"></i></a></li>
+                                <li className="list-inline-item mb-0"><a href="#" target="_blank" className="rounded"><i className="uil uil-behance align-middle" title="behance"></i></a></li>
+                                <li className="list-inline-item mb-0"><a href="#" target="_blank" className="rounded"><i className="uil uil-facebook-f align-middle" title="facebook"></i></a></li>
+                                <li className="list-inline-item mb-0"><a href="#" target="_blank" className="rounded"><i className="uil uil-instagram align-middle" title="instagram"></i></a></li>
+                                <li className="list-inline-item mb-0"><a href="#" target="_blank" className="rounded"><i className="uil uil-twitter align-middle" title="twitter"></i></a></li>
+                                <li className="list-inline-item mb-0"><a href="#" className="rounded"><i className="uil uil-envelope align-middle" title="email"></i></a></li>
                             </ul>
                         </div>
                     </div>{/*end col*/}
