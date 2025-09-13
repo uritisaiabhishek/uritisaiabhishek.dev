3f553b92e7b02d2fc6199a4221f01b0967499c83
uritisaiabhishek <uritisaiabhishek@gmail.com>
2025-09-13 20:41:32 +0530

    removed blog link in nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,9 +25,6 @@ function Header() {
           <NavLink className="nav-link" to="/about">About</NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" to="/blog">Blog</NavLink>
-        </li>
-        <li className="nav-item">
           <NavLink className="nav-link" to="/portfolio">Portfolio</NavLink>
         </li>
         <li className="nav-item">
